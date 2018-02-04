632a5edd8622ede8b56e0f305bbd74e869e40c96
= <=>
2018-02-04 19:39:54 +0100

    Bitmap of current chord

--- GuitarConcert/Song/Chord.cs
+++ GuitarConcert/Song/Chord.cs
@@ -36,11 +36,11 @@ namespace GuitarConcert
 			RestoreDefault();
 		}
 		
-		public void Load(string chordName)
+		public void Load(string fileName)
 		{
 			try {
 				XmlDocument doc = new XmlDocument();
-				doc.LoadXml(File.ReadAllText(PathGenerator.ChordDiagramPath(chordName)));
+				doc.LoadXml(File.ReadAllText(PathGenerator.ChordDiagramPath(fileName)));
 				
 				this.Name = doc.GetElementsByTagName("chord")[0].Attributes[0].Value;
 				
@@ -59,13 +59,29 @@ namespace GuitarConcert
 		
 		public Bitmap CreateBitmap()
 		{
-			Bitmap bmp = new Bitmap(300, 150);
+			Bitmap bmp = new Bitmap(300, 200);
 			Graphics graphics = Graphics.FromImage(bmp);
 			Pen blackPen = new Pen(Color.Black);
 			
-			// TODO Draw chord diagram
+			graphics.DrawString(this.Name, new Font(SettingsSingleton.Instance.option["fontName"], float.Parse(SettingsSingleton.Instance.option["fontSize"]), System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238))), Brushes.Black, 120, 0);
+			graphics.DrawRectangle(new Pen(Color.Black), 0, 30, 290, 125);
 			
-			graphics.DrawString(this.Name, new Font(SettingsSingleton.Instance.option["fontName"], float.Parse(SettingsSingleton.Instance.option["fontSize"]), System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238))), Brushes.Black, 0, 0);
+			for(int y=48; y<150; y+=18)
+			{
+				graphics.DrawLine(new Pen(Color.Chocolate), 0, y, 290, y);
+			}
+			
+			for(int x=72; x<290; x+=73)
+			{
+				graphics.DrawLine(new Pen(Color.DarkKhaki), x, 30, x, 155);
+			}
+			
+			graphics.FillEllipse(new SolidBrush(Color.Red), E4==0? -4:(E4-1)*72+36-5, 30+(18*1)-5, 10, 10);
+			graphics.FillEllipse(new SolidBrush(Color.Red), B3==0? -4:(B3-1)*72+36-5, 30+(18*2)-5, 10, 10);
+			graphics.FillEllipse(new SolidBrush(Color.Red), G3==0? -4:(G3-1)*72+36-5, 30+(18*3)-5, 10, 10);
+			graphics.FillEllipse(new SolidBrush(Color.Red), D3==0? -4:(D3-1)*72+36-5, 30+(18*4)-5, 10, 10);
+			graphics.FillEllipse(new SolidBrush(Color.Red), A2==0? -4:(A2-1)*72+36-5, 30+(18*5)-5, 10, 10);
+			graphics.FillEllipse(new SolidBrush(Color.Red), E2==0? -4:(E2-1)*72+36-5, 30+(18*6)-5, 10, 10);
 			
 			return bmp;
 		}
