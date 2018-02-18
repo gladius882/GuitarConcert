c421365dfc50c031e8bc5a3e9f0a179b0a713e4f
gladius882 <=>
2018-02-18 21:16:25 +0100

    MainForm toolStrip instead menuStrip

--- GuitarConcert/MainForm.Designer.cs
+++ GuitarConcert/MainForm.Designer.cs
@@ -36,68 +36,48 @@ namespace GuitarConcert
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.menuStrip2 = new System.Windows.Forms.MenuStrip();
-			this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.widokToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.ustawieniaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.informacjeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.menuStrip2.SuspendLayout();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.toolStrip1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// menuStrip2
-			// 
-			this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-			this.plikToolStripMenuItem,
-			this.widokToolStripMenuItem1,
-			this.ustawieniaToolStripMenuItem1,
-			this.informacjeToolStripMenuItem1});
-			this.menuStrip2.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip2.Name = "menuStrip2";
-			this.menuStrip2.Size = new System.Drawing.Size(845, 24);
-			this.menuStrip2.TabIndex = 2;
-			this.menuStrip2.Text = "menuStrip2";
-			// 
-			// plikToolStripMenuItem
-			// 
-			this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
-			this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
-			this.plikToolStripMenuItem.Text = "Plik";
-			// 
-			// widokToolStripMenuItem1
-			// 
-			this.widokToolStripMenuItem1.Name = "widokToolStripMenuItem1";
-			this.widokToolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
-			this.widokToolStripMenuItem1.Text = "Widok";
-			// 
-			// ustawieniaToolStripMenuItem1
-			// 
-			this.ustawieniaToolStripMenuItem1.Name = "ustawieniaToolStripMenuItem1";
-			this.ustawieniaToolStripMenuItem1.Size = new System.Drawing.Size(76, 20);
-			this.ustawieniaToolStripMenuItem1.Text = "Ustawienia";
-			// 
-			// informacjeToolStripMenuItem1
-			// 
-			this.informacjeToolStripMenuItem1.Name = "informacjeToolStripMenuItem1";
-			this.informacjeToolStripMenuItem1.Size = new System.Drawing.Size(76, 20);
-			this.informacjeToolStripMenuItem1.Text = "Informacje";
 			// 
 			// menuStrip1
 			// 
-			this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
 			this.menuStrip1.Size = new System.Drawing.Size(845, 24);
 			this.menuStrip1.TabIndex = 0;
 			this.menuStrip1.Text = "menuStrip1";
 			this.menuStrip1.Visible = false;
 			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(845, 25);
+			this.toolStrip1.TabIndex = 4;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton1.Text = "toolStripButton1";
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(845, 256);
+			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.menuStrip1);
-			this.Controls.Add(this.menuStrip2);
 			this.IsMdiContainer = true;
 			this.KeyPreview = true;
 			this.MainMenuStrip = this.menuStrip1;
@@ -105,17 +85,14 @@ namespace GuitarConcert
 			this.Text = "GuitarConcert";
 			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainFormKeyPress);
-			this.menuStrip2.ResumeLayout(false);
-			this.menuStrip2.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
 		}
-		private System.Windows.Forms.ToolStripMenuItem informacjeToolStripMenuItem1;
-		private System.Windows.Forms.ToolStripMenuItem ustawieniaToolStripMenuItem1;
-		private System.Windows.Forms.ToolStripMenuItem widokToolStripMenuItem1;
-		private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
-		private System.Windows.Forms.MenuStrip menuStrip2;
 		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
 	}
 }
